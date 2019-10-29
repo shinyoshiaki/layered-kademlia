9a816c609f502009e6d2fbf2a8644c7260883fdf
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-10-29 14:59:01 +0900

    update example

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -2,7 +2,8 @@ import React, { createContext, useEffect, useRef, useState } from "react";
 
 import PeerList from "./components/PeerList";
 import { SP2PClient } from "./services/kademlia";
-import { StaticMeta } from "../../../src/entity/data/meta";
+import SeederList from "./components/SeederList";
+import { StaticMeta } from "../../../src/sp2p/entity/data/meta";
 import StoreStream from "./components/StoreStream";
 import WatchStream from "./components/WatchStream";
 import useInput from "./hooks/useInput";
@@ -41,6 +42,7 @@ const App: React.FC = () => {
       <p>sp2p</p>
       <p>{sP2PClient.kad.kid}</p>
       <PeerList />
+      <SeederList />
       <StoreStream />
       <WatchStream />
     </SP2PClientContext.Provider>
--- examples/react/src/components/WatchStream.tsx
+++ examples/react/src/components/WatchStream.tsx
@@ -1,22 +1,28 @@
-import React, { useContext, useRef } from "react";
+import React, { FC, useContext, useRef, useState } from "react";
 
 import { SP2PClientContext } from "../App";
-import { StreamMeta } from "../../../../src/entity/data/meta";
+import { StreamMeta } from "../../../../src/sp2p/entity/data/meta";
+import { StreamPlayer } from "../domain/stream/que";
 import { VideoCanvas } from "../atoms/VideoCanvas";
 import { decode } from "@msgpack/msgpack";
 import { libvpxDec } from "../domain/libvpx";
 import useInput from "../hooks/useInput";
 
-const WatchStream: React.FC = () => {
+const WatchStream: FC = () => {
+  const [resolution, setResolution] = useState({ x: 400, y: 400 });
   const [url, seturl] = useInput();
   const canvasRef = useRef<any>();
   const sp2pClient = useContext(SP2PClientContext);
 
   const watch = async () => {
-    const res = await sp2pClient.actor.user.connectSubNet(url).catch(() => {});
-    if (!res) return;
+    const res = await sp2pClient.actor.user
+      .connectSubNet(url)
+      .catch(console.warn);
+    if (!res) throw new Error("meta not found");
+
     const { subNet, meta } = res;
     const { width, height } = (meta as StreamMeta).payload;
+    setResolution({ x: width, y: height });
     console.log({ meta });
     const { sender, listener } = await libvpxDec({
       codec: "VP8",
@@ -27,24 +33,30 @@ const WatchStream: React.FC = () => {
       packetSize: 1
     });
 
-    subNet.findStreamMetaTarget(meta as StreamMeta, ({ type, chunk }) => {
-      if (type === "chunk") {
-        const { video } = decode(chunk) as {
-          video: Uint8Array;
-          audio: Uint8Array[];
-        };
-        console.log({ video });
-        sender.execute(new Uint8Array(Object.values(video)).buffer);
-      }
-    });
-
     listener.subscribe(ab => {
       const ctx = canvasRef.current.getContext("2d");
       const frame = ctx.createImageData(width, height);
       frame.data.set(ab, 0);
       ctx.putImageData(frame, 0, 0);
     });
+
+    const streamPlayer = new StreamPlayer();
+    streamPlayer.event.subscribe(async v => {
+      sender.execute(new Uint8Array(Object.values(v)).buffer);
+    });
+
+    subNet.findStreamMetaTarget(meta as StreamMeta, async ({ type, chunk }) => {
+      if (type === "chunk") {
+        const { video } = decode(chunk) as {
+          video: Uint8Array[];
+          audio: Uint8Array[];
+        };
+        streamPlayer.push(video.map(v => decode(v) as any));
+      }
+    });
   };
+
+  const { x, y } = resolution;
 
   return (
     <div>
@@ -53,7 +65,7 @@ const WatchStream: React.FC = () => {
       <VideoCanvas
         canvasRef={canvasRef}
         style={{ width: 400, height: 400 }}
-        source={{ width: 400, height: 400 }}
+        source={{ width: x, height: y }}
       />
     </div>
   );
