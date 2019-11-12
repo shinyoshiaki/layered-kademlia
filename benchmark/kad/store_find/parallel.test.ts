bc07d7400f3db619c90c5ca5fcccab97b02c9b09
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-11-12 16:47:51 +0900

    fix jest test

--- benchmark/kad/store_find/parallel.test.ts
+++ benchmark/kad/store_find/parallel.test.ts
@@ -4,36 +4,40 @@ import { testSetupNodes } from "../../../src/tests/setupnetwork";
 const NODE_NUM = 50;
 const log = (...s: any[]) => console.log(`kad/store_find/parallel `, ...s);
 
-test("kad/store_find/parallel", async () => {
-  const start = Date.now();
-  log("kad bench");
+test(
+  "kad/store_find/parallel",
+  async () => {
+    const start = Date.now();
+    log("kad bench");
 
-  const nodes = await testSetupNodes(NODE_NUM, PeerMockModule, {
-    timeout: 10_000
-  });
-  log("node setup");
+    const nodes = await testSetupNodes(NODE_NUM, PeerMockModule, {
+      timeout: 10_000
+    });
+    log("node setup");
 
-  const urls = (
-    await Promise.all(
-      nodes.map(async node => {
-        const res = await node.store(node.kid).catch(() => {});
-        if (res) return res.item.key;
-      })
-    )
-  ).filter(v => !!v) as string[];
-  log("store", urls.length);
+    const urls = (
+      await Promise.all(
+        nodes.map(async node => {
+          const res = await node.store(node.kid).catch(() => {});
+          if (res) return res.item.key;
+        })
+      )
+    ).filter(v => !!v) as string[];
+    log("store", urls.length);
 
-  const values = (
-    await Promise.all(
-      nodes.map(async (node, i) => {
-        const res = await node
-          .findValue(urls[urls.length - i - 1])
-          .catch(() => {});
-        if (res) return res.item.value;
-      })
-    )
-  ).filter(v => !!v);
-  log("findvalue", values.length);
+    const values = (
+      await Promise.all(
+        nodes.map(async (node, i) => {
+          const res = await node
+            .findValue(urls[urls.length - i - 1])
+            .catch(() => {});
+          if (res) return res.item.value;
+        })
+      )
+    ).filter(v => !!v);
+    log("findvalue", values.length);
 
-  log("kad end bench", (Date.now() - start) / 1000 + "s");
-});
+    log("kad end bench", (Date.now() - start) / 1000 + "s");
+  },
+  Infinity
+);
--- benchmark/layered/store_find/index.test.ts
+++ benchmark/layered/store_find/index.test.ts
@@ -6,39 +6,43 @@ import { testSetupNodes } from "../../../src/tests/setupnetwork";
 const NODE_NUM = 50;
 const log = (...s: any[]) => console.log(`layered/store_find `, ...s);
 
-test("layered/store_find", async () => {
-  console.log("layerd Kad");
-  const start = Date.now();
-  const nodes = await testSetupNodes(NODE_NUM, PeerMockModule, {
-    timeout: 10_000
-  });
-  const actors = nodes.map(
-    node => new SP2P({ PeerCreater: new PeerCreater(PeerMockModule) }, node)
-  );
-  const urls = (
-    await Promise.all(
-      actors.map(async actor => {
-        const res = await actor.seeder
-          .storeStatic(actor.mainNet.kid, Buffer.from(actor.mainNet.kid))
-          .catch(() => {});
-        if (res) return res.url;
-      })
-    )
-  ).filter(v => !!v) as string[];
-  log("store", urls.length);
+test(
+  "layered/store_find",
+  async () => {
+    console.log("layerd Kad");
+    const start = Date.now();
+    const nodes = await testSetupNodes(NODE_NUM, PeerMockModule, {
+      timeout: 10_000
+    });
+    const actors = nodes.map(
+      node => new SP2P({ PeerCreater: new PeerCreater(PeerMockModule) }, node)
+    );
+    const urls = (
+      await Promise.all(
+        actors.map(async actor => {
+          const res = await actor.seeder
+            .storeStatic(actor.mainNet.kid, Buffer.from(actor.mainNet.kid))
+            .catch(() => {});
+          if (res) return res.url;
+        })
+      )
+    ).filter(v => !!v) as string[];
+    log("store", urls.length);
 
-  const values = (
-    await Promise.all(
-      actors.map(async (actor, i) => {
-        const res = await actor.user
-          .findStatic(urls[urls.length - i - 1])
-          .catch(() => {});
-        if (res) return res;
-      })
-    )
-  ).filter(v => !!v) as ArrayBuffer[];
+    const values = (
+      await Promise.all(
+        actors.map(async (actor, i) => {
+          const res = await actor.user
+            .findStatic(urls[urls.length - i - 1])
+            .catch(() => {});
+          if (res) return res;
+        })
+      )
+    ).filter(v => !!v) as ArrayBuffer[];
 
-  log("findvalue", values.length);
+    log("findvalue", values.length);
 
-  log("layerd Kad end bench", (Date.now() - start) / 1000 + "s");
-});
+    log("layerd Kad end bench", (Date.now() - start) / 1000 + "s");
+  },
+  Infinity
+);
