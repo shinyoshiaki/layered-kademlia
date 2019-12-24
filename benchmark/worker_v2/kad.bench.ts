78843e59515b7f204c627ad3721be55d7acb9dde
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-12-24 20:24:14 +0900

    TIMEOUT = 5_000

--- benchmark/worker_v2/kad.bench.ts
+++ benchmark/worker_v2/kad.bench.ts
@@ -3,6 +3,6 @@ import { kadBench } from "./kad";
 const NODE_NUM = 16;
 const GROUP_NUM = NODE_NUM / 2;
 const VALUE = "benchmark";
-export const TIMEOUT = 60_000;
+export const TIMEOUT = 5_000;
 
 kadBench(NODE_NUM, GROUP_NUM, VALUE);
