8174e022423b5daacc1ec4c946f3767f619f8541
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-12-24 20:36:57 +0900

    5

--- benchmark/worker_v2/layered.bench.ts
+++ benchmark/worker_v2/layered.bench.ts
@@ -2,6 +2,6 @@ import { layeredBench } from "./layered";
 
 const NODE_NUM = 16;
 const GROUP_NUM = NODE_NUM / 2;
-const VALUE = "123456789";
+const VALUE = "12345";
 
 layeredBench(NODE_NUM, GROUP_NUM, VALUE);
