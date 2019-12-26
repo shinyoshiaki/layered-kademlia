d3666095bfc9cc888745c51e520298602a436c78
shinyoshiaki <shinyoshiaki2012@gmail.com>
2019-12-26 13:24:31 +0900

    const VALUE = "1234";

--- benchmark/worker_v2/layered.bench.ts
+++ benchmark/worker_v2/layered.bench.ts
@@ -2,6 +2,6 @@ import { layeredBench } from "./layered";
 
 const NODE_NUM = 16;
 const GROUP_NUM = NODE_NUM / 2;
-const VALUE = "12345";
+const VALUE = "1234";
 
 layeredBench(NODE_NUM, GROUP_NUM, VALUE);
