6030919b68e393703713b71fcc7479b2ce64de97
shinyoshiaki <shinyoshiaki2012@gmail.com>
2020-01-16 18:25:51 +0900

    fix traffic bench

--- benchmark/traffic/traffic.bench_.ts
+++ benchmark/traffic/traffic.bench_.ts
@@ -1,22 +1,13 @@
-import {
-  getTrafficContextTraffic,
-  resetTrafficContext
-} from "../mock/peer/traffic";
-
 import { benchmarkKadTraffic } from "./kad";
 import { benchmarkLayeredTraffic } from "./layered";
+import { resetTrafficContext } from "../mock/peer/traffic";
 
 const NODE_NUM = 20;
 const GROUP_NUM = NODE_NUM / 2;
 
-test(
-  "mix/traffic",
-  async () => {
-    await benchmarkLayeredTraffic(NODE_NUM, GROUP_NUM);
-    resetTrafficContext();
-    console.log("reset", getTrafficContextTraffic());
-    await benchmarkKadTraffic(NODE_NUM, GROUP_NUM);
-    await new Promise(r => setTimeout(r, 2000));
-  },
-  60_000 * 60 * 24
-);
+(async () => {
+  await benchmarkLayeredTraffic(NODE_NUM, GROUP_NUM);
+  resetTrafficContext();
+  await benchmarkKadTraffic(NODE_NUM, GROUP_NUM);
+  await new Promise(r => setTimeout(r, 2000));
+})();
