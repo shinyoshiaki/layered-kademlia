2fc701c07cb65fe7bb4f0975bc6ce1a738fdffda
shinyoshiaki <shinyoshiaki2012@gmail.com>
2020-01-29 17:37:30 +0900

    update worker bench

--- benchmark/worker/kad.bench.ts
+++ benchmark/worker/kad.bench.ts
@@ -2,6 +2,9 @@ import { kadBench } from "./kad";
 
 const NODE_NUM = 16;
 const GROUP_NUM = NODE_NUM / 2;
-const VALUE = "123456";
 
-kadBench(NODE_NUM, GROUP_NUM, VALUE);
+(async () => {
+  for (let VALUE = "1", i = 0; i <= 6; i++, VALUE += "1") {
+    await kadBench(NODE_NUM, GROUP_NUM, VALUE);
+  }
+})();
--- benchmark/worker/layered.bench.ts
+++ benchmark/worker/layered.bench.ts
@@ -2,6 +2,9 @@ import { layeredBench } from "./layered";
 
 const NODE_NUM = 16;
 const GROUP_NUM = NODE_NUM / 2;
-const VALUE = "123456";
 
-layeredBench(NODE_NUM, GROUP_NUM, VALUE);
+(async () => {
+  for (let VALUE = "1", i = 0; i <= 6; i++, VALUE += "1") {
+    await layeredBench(NODE_NUM, GROUP_NUM, VALUE);
+  }
+})();
