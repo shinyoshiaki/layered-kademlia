51119a2f30446f9733d7441ff3629b2249c48346
shinyoshiaki <shinyoshiaki2012@gmail.com>
2020-01-30 17:30:50 +0900

    use param

--- benchmark/worker/kad.bench.ts
+++ benchmark/worker/kad.bench.ts
@@ -1,8 +1,5 @@
 import { kadBench } from "./kad";
-
-const NODE_NUM = 16;
-const GROUP_NUM = NODE_NUM / 2;
-const VALUE_LENGTH = 1;
+import { VALUE_LENGTH, GROUP_NUM, NODE_NUM } from "./param";
 
 const VALUE = [...Array(VALUE_LENGTH)].map(() => "1").join("");
 
